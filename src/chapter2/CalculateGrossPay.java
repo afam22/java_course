c8261e68b388101afd76e4fe4c0d71ea12ecebf7
Afam <happyguy_afam@yahoo.com>
2021-07-08 16:53:18 +0100

    Calculate gross pay

---
+++ src/chapter2/CalculateGrossPay.java
@@ -0,0 +1,23 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class CalculateGrossPay {
+    public static void main(String[] args) {
+//        1. get hour worked
+        System.out.println("Please enter hours worked: ");
+        Scanner scanner = new Scanner(System.in);
+        int hour = scanner.nextInt();
+
+//        2. get hourly rate
+        System.out.println("Please enter hourly rate: ");
+        double rate = scanner.nextDouble();
+        scanner.close();
+
+//        3. multiply hour and rate
+        double grossPay = hour * rate;
+
+//        4. display gross pay
+        System.out.println("Gross pay = $" + grossPay);
+    }
+}
