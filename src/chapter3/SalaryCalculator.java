a27ebe5bf8dbbacf3faf268ee0f76a50a4af491f
Afam <happyguy_afam@yahoo.com>
2021-07-08 17:58:05 +0100

    added season sentence assignment

---
+++ src/chapter3/SalaryCalculator.java
@@ -0,0 +1,31 @@
+package chapter3;
+
+import java.util.Scanner;
+
+/*
+If statement.
+All sales get a payment of $1,000 a week.
+Sales people that exceed 10 sales get an additional $250 bonus.
+*/
+public class SalaryCalculator {
+    public static void main(String[] args) {
+        //initialize known values
+        double salary = 1000;
+        double bonus = 250;
+
+        // get values for the unknown
+        System.out.println("How many sales did the employee make this week?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        //Quick detour for the bonus earners
+        if(sales > 10){
+            salary = salary + bonus;
+        }
+
+        //Output
+        System.out.println("The employee's pay is $" + salary);
+
+    }
+}
