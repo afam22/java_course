fcd298cb742079032db87baee85e6bec6636cfb3
Afam <happyguy_afam@yahoo.com>
2021-07-09 10:32:24 +0100

    added while loop(Salary example) - chapter 4A

---
+++ src/chapter4/GrossPayInputValidation.java
@@ -0,0 +1,30 @@
+package chapter4;
+
+import java.util.Scanner;
+
+/*
+* WHILE LOOP
+* Each store employee makes $15 an hour. Write a program that allows the employee
+* to enter the number of hours worked for the week. Do not allow overtime.
+*/
+public class GrossPayInputValidation {
+    public static void main(String[] args) {
+        int rate = 15;
+        int maxhours= 40;
+
+        System.out.println("How many hours did you work this week");
+        Scanner scanner = new Scanner(System.in);
+        double hoursWorked = scanner.nextDouble();
+
+        //validate input
+        while (hoursWorked > maxhours || hoursWorked < 1){
+            System.out.println("Invalid entry.. Your hours must be between 1 and 40. Try again");
+            hoursWorked = scanner.nextDouble();
+        }
+        scanner.close();
+        double gross = rate * hoursWorked;
+
+        System.out.println("Your gross pay is $" + gross);
+
+    }
+}
